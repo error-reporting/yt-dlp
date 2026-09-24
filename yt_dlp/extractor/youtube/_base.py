a2da76d88b46b1fc5b079e865d5cd394a7bf2b95
error-reporting <huynhnhuthoang5@gmail.com>
2026-09-24 08:45:14 +0700

    Update _base.py

--- yt_dlp/extractor/youtube/_base.py
+++ yt_dlp/extractor/youtube/_base.py
@@ -315,9 +315,7 @@ INNERTUBE_CLIENTS = {
             'client': {
                 'clientName': 'MWEB',
                 'clientVersion': '2.20260708.05.00',
-                # mweb previously did not require PO Token with this UA
-                'userAgent': 'Mozilla/5.0 (iPad; CPU OS 16_7_10 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1,gzip(gfe)',
-            },
+                },
         },
         'INNERTUBE_CONTEXT_CLIENT_NAME': 2,
         'GVS_PO_TOKEN_POLICY': {
